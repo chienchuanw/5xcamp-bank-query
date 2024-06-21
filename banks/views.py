a8fd2dc4b53422cf1020bd02a16163cc031e1ed4
Chien Chuan Wang <chien@chienchuan.com>
2024-06-21 21:56:34 +0800

    Modified: Added bank dropdown options

--- banks/views.py
+++ banks/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from typing import Any
+from django.db.models import Q
+from django.db.models.query import QuerySet
+from django.views.generic import FormView, ListView
+from .models import Bank
+from .forms import BankSearchForm
 
 
-class BankHomeView(TemplateView):
+class BankHomeView(ListView):
     template_name = "banks/home.html"
+    model = Bank
+    context_object_name = "banks"
