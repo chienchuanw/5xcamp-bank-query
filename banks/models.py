aef089205540176938a073bc214275fe0d4e588b
Chien Chuan Wang <chien@chienchuan.com>
2024-06-21 15:57:35 +0800

    Modified: Created class 'Bank' in models.py and registered it in admin

--- banks/models.py
+++ banks/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Bank(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=10)
+    branch_name = models.CharField(max_length=100)
+    branch_code = models.CharField(max_length=10)
+    address = models.CharField(max_length=200)
+    telephone = models.CharField(max_length=20)
+
+    class Meta:
+        verbose_name = "Bank"
+        verbose_name_plural = "Banks"
+
+    def __str__(self) -> str:
+        return f"{self.code} - {self.name}{self.branch_name}"
