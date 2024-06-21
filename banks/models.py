bc7ef12099195f2ec5913e2e6add850540826bde
Chien Chuan Wang <chien@chienchuan.com>
2024-06-21 18:12:35 +0800

    Modified: Added class Branch in order to organize the database

--- banks/models.py
+++ banks/models.py
@@ -4,14 +4,29 @@ from django.db import models
 class Bank(models.Model):
     name = models.CharField(max_length=100)
     code = models.CharField(max_length=10)
-    branch_name = models.CharField(max_length=100)
-    branch_code = models.CharField(max_length=10)
-    address = models.CharField(max_length=200)
-    telephone = models.CharField(max_length=20)
 
     class Meta:
         verbose_name = "Bank"
         verbose_name_plural = "Banks"
 
     def __str__(self) -> str:
-        return f"{self.code} - {self.name}{self.branch_name}"
+        return f"{self.code} - {self.name}"
+
+
+class Branch(models.Model):
+    bank = models.ForeignKey(Bank, on_delete=models.CASCADE, related_name="branches")
+    branch_name = models.CharField(max_length=100)
+    branch_code = models.CharField(max_length=10)
+    address = models.CharField(max_length=200)
+    telephone = models.CharField(max_length=20)
+    website = models.URLField(max_length=200, null=True, blank=True)
+    manager = models.CharField(max_length=100, null=True, blank=True)
+    update_date = models.DateField(null=True, blank=True)
+    announcement_date = models.DateField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Branch"
+        verbose_name_plural = "Branches"
+
+    def __str__(self) -> str:
+        return f"{self.branch_code} - {self.branch_name}"
