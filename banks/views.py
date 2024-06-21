6c6556e24a8e57e34382715558d94c590663ceb2
Chien Chuan Wang <chien@chienchuan.com>
2024-06-21 22:49:24 +0800

    Modified: Added branch dropdown options

--- banks/views.py
+++ banks/views.py
@@ -1,12 +1,15 @@
 from typing import Any
-from django.db.models import Q
-from django.db.models.query import QuerySet
-from django.views.generic import FormView, ListView
-from .models import Bank
-from .forms import BankSearchForm
+from django.views.generic import ListView
+from .models import Bank, Branch
 
 
 class BankHomeView(ListView):
     template_name = "banks/home.html"
     model = Bank
     context_object_name = "banks"
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["branches"] = Branch.objects.all()
+
+        return context
