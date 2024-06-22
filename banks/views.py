c699285a67342f54b5200163bab314ee46fc28f2
Chien Chuan Wang <chien@chienchuan.com>
2024-06-22 21:36:37 +0800

    Modified: Serialized banks and branches querysets to JSON

--- banks/views.py
+++ banks/views.py
@@ -1,6 +1,7 @@
 from typing import Any
 from django.views.generic import ListView
 from .models import Bank, Branch
+import json
 
 
 class BankHomeView(ListView):
@@ -11,5 +12,15 @@ class BankHomeView(ListView):
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context["branches"] = Branch.objects.all()
+
+        # Serialize the banks and branches querysets to JSON
+        banks_json = json.dumps(list(Bank.objects.values("id", "code", "name")))
+        branches_json = json.dumps(
+            list(Branch.objects.values("id", "branch_name", "bank_id"))
+        )
+
+        # Add the JSON serialized data to the context
+        context["banks_json"] = banks_json
+        context["branches_json"] = branches_json
 
         return context
