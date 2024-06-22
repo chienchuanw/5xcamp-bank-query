f061e9c3d859be5c6f4cff691252fbc8fda39483
Chien Chuan Wang <chien@chienchuan.com>
2024-06-23 02:28:09 +0800

    Modified: Trimmed bank name and branch name

--- banks/views.py
+++ banks/views.py
@@ -1,5 +1,5 @@
 from typing import Any
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Bank, Branch
 import json
 from django.conf import settings
@@ -14,8 +14,20 @@ class BankHomeView(ListView):
         context = super().get_context_data(**kwargs)
         context["branches"] = Branch.objects.all()
 
+        banks = Bank.objects.values("id", "code", "name")
+
+        # Process each bank to remove substrings from the name
+        renamed_bank = [
+            {
+                "id": bank["id"],
+                "code": bank["code"],
+                "name": bank["name"].replace("股份有限公司", "").strip(),
+            }
+            for bank in banks
+        ]
+
         # Serialize the banks and branches querysets to JSON
-        banks_json = json.dumps(list(Bank.objects.values("id", "code", "name")))
+        banks_json = json.dumps(renamed_bank)
         branches_json = json.dumps(
             list(
                 Branch.objects.values(
@@ -32,7 +44,10 @@ class BankHomeView(ListView):
         # Add the JSON serialized data to the context
         context["banks_json"] = banks_json
         context["branches_json"] = branches_json
-
         context["base_url"] = settings.BASE_URL
 
         return context
+
+
+class BranchDetailView(DetailView):
+    pass
